4144d50e0b1840fede5bcca2665d659dde2433a4
dongdapixiu <79754721+dongdapixiu@users.noreply.github.com>
2021-02-27 23:52:25 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,19 @@
+#include "cpp_std.h"
+#include <stdio.h>
+#include <windows.h>
+#include <iostream>
+//using namespace std;
+//using std::cout;
+//using std::cin;
+void func(int a=0,int b=0,int c=0)
+{
+
+	return;
+}
+int main()
+{
+	printf("hellow world\n");
+	std::cout << 1 << std::endl;
+	system("pause");
+	return 0;
+}
